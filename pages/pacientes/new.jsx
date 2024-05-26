3a1160843bfebc2db7d45b901557d8e9e77523f0
SrChey <carlos-avb@hotmail.com>
2024-05-25 19:34:40 -0500

    idiomas en pacientes

--- pages/pacientes/new.jsx
+++ pages/pacientes/new.jsx
@@ -1,10 +1,12 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import MenuWrapper from '@/components/sidebar';
 import FormPaciente from '@/components/pacientes/patientForm';
 import { useUserContext } from '@/assets/useUserContext';
 import { toIndex } from '@/utils/toindex/toindex';
+import { informacionDelPaciente as esp } from "@/assets/lenguajes/esp.js";
+import { informacionDelPaciente as eng } from "@/assets/lenguajes/eng.js";
 
 export default function NuevoPaciente() {
     const { user } = useUserContext();
@@ -12,7 +14,9 @@ export default function NuevoPaciente() {
     useEffect(() => {
         toIndex(user);
     }, [user]);
-    return <MenuWrapper>
-        <FormPaciente />
+    const [lang, setLang] = useState(esp);
+
+    return <MenuWrapper setLang={setLang} esp={esp} eng={eng}>
+        <FormPaciente lang={lang} />
     </MenuWrapper>;
 }
