d748f0443785640cd7866b114da6b364215dd06b
SrChey <carlos-avb@hotmail.com>
2024-08-01 18:45:32 -0500

    cambios

--- components/commons/breadCrumPaciente.jsx
+++ components/commons/breadCrumPaciente.jsx
@@ -1,57 +1,69 @@
 import { useUserContext } from "@/assets/useUserContext";
-import { Breadcrumb, } from "antd";
+import { Breadcrumb } from "antd";
 import useTranslation from 'next-translate/useTranslation';
 
 export default function BreadCrumbPacientes({ idPaciente, page }) {
     const { t } = useTranslation('home');
     const { user } = useUserContext();
 
-    if (idPaciente) {
-        var items = [
-            { title: t('Dashboard'), href: '/dashboard' },
-            { title: t('Pacientes'), href: `/pacientes/` },
-            {
-                title: page, menu: {
-                    items: [
-                        {
-                            label: (
-                                <a rel="noopener noreferrer" href={`/pacientes/${idPaciente}`}>
-                                    {t('VerPaciente')}
-                                </a>
-                            ),
-                        },
-                        {
-                            label: (
-                                <a rel="noopener noreferrer" href={`/pacientes/edit/${idPaciente}`}>
-                                    {t('EditarPaciente')}
-                                </a>
-                            ),
-                        },
-                        {
-                            label: (
-                                <a rel="noopener noreferrer" href={`/pacientes/seguimientos/${idPaciente}`}>
-                                    {t('SeguimientosPacientes')}
-                                </a>
-                            ),
-                        },
-                        {
-                            label: (
-                                <a rel="noopener noreferrer" href={`/pacientes/tests/${idPaciente}`}>
-                                    {t('TestPacientes')}
-                                </a>
-                            ),
-                        },
-                    ]
-                }
-            }
+    let items = [
+        { title: t('Dashboard'), href: '/dashboard' },
+        { title: t('Pacientes'), href: `/pacientes/` },
+    ];
 
-        ]
+    if (idPaciente) {
+        const patientMenuItems = [
+            {
+                label: (
+                    <a rel="noopener noreferrer" href={`/pacientes/${idPaciente}`}>
+                        {t('VerPaciente')}
+                    </a>
+                ),
+            },
+            {
+                label: (
+                    <a rel="noopener noreferrer" href={`/pacientes/edit/${idPaciente}`}>
+                        {t('EditarPaciente')}
+                    </a>
+                ),
+            },
+            {
+                label: (
+                    <a rel="noopener noreferrer" href={`/pacientes/seguimientos/${idPaciente}`}>
+                        {t('SeguimientosPacientes')}
+                    </a>
+                ),
+            },
+            {
+                label: (
+                    <a rel="noopener noreferrer" href={`/pacientes/tests/${idPaciente}`}>
+                        {t('TestPacientes')}
+                    </a>
+                ),
+            },
+        ];
+
+        if (user?.permisos["pacientes"]) {
+            patientMenuItems.push({
+                label: (
+                    <a rel="noopener noreferrer" href={`/pacientes/cambios/${idPaciente}`}>
+                        {t('Cambios')}
+                    </a>
+                ),
+            });
+        }
+
+        items.push({
+            title: page,
+            menu: {
+                items: patientMenuItems
+            }
+        });
     } else {
-        var items = [
-            { title: 'Dashboard', href: '/dashboard' },
-            { title: 'Pacientes', href: `/pacientes/` },
-            user.permisos["pacientes"] ? {
-                title: page, menu: {
+        if (user.permisos["pacientes"]) {
+            items.push({
+                title: page,
+                menu: {
                     items: [
                         {
                             label: (
@@ -62,9 +74,11 @@ export default function BreadCrumbPacientes({ idPaciente, page }) {
                         }
                     ]
                 }
-            } : { title: page }
-
-        ]
+            });
+        } else {
+            items.push({ title: page });
+        }
     }
-    return <Breadcrumb items={items} />
+
+    return <Breadcrumb items={items} />;
 }
---
+++ pages/pacientes/cambios/[id].jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import { Table, Typography, Collapse } from 'antd';
+import axios from 'axios';
+import MenuWrapper from '@/components/sidebar';
+import BreadCrumbPacientes from '@/components/commons/breadCrumPaciente';
+import useTranslation from 'next-translate/useTranslation';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
+
+const renderValue = (value) => {
+    if (typeof value === 'object' && value !== null) {
+        return (
+            <Table
+                dataSource={Object.keys(value).map((key, index) => ({
+                    key: index,
+                    field: key,
+                    value: value[key],
+                }))}
+                columns={[
+                    { title: 'Field', dataIndex: 'field', key: 'field' },
+                    { title: 'Value', dataIndex: 'value', key: 'value', render: renderValue },
+                ]}
+                pagination={false}
+                showHeader={false}
+                rowKey="field"
+            />
+        );
+    } else {
+        return value;
+    }
+};
+const isDifferent = (value, compareValue) => {
+    console.log(value, compareValue, value !== compareValue);
+
+    if (typeof value === 'object' && value !== null) {
+        return Object.keys(value).some(key => isDifferent(value[key], compareValue[key]));
+    } else {
+        return value !== compareValue;
+    }
+}
+const renderJsonCell = (jsonString, compareJsonString) => {
+    try {
+        const jsonObject = jsonString ? JSON.parse(jsonString) : {};
+        const compareJsonObject = compareJsonString ? JSON.parse(compareJsonString) : {};
+
+        return (
+            <Table
+                dataSource={Object.keys(jsonObject).map((key, index) => ({
+                    key: index,
+                    field: key,
+                    value: jsonObject[key],
+                }))}
+                columns={[
+                    { title: 'Field', dataIndex: 'field', key: 'field' },
+                    {
+                        title: 'Value',
+                        dataIndex: 'value',
+                        key: 'value',
+                        render: (text, record) => (
+                            <span style={{ backgroundColor: isDifferent(record.value, record.value) ? 'yellow' : 'inherit' }}>
+                                {renderValue(text)}
+                            </span>
+                        )
+                    },
+                ]}
+                pagination={{ defaultPageSize: 5, showSizeChanger: false, pageSizeOptions: ['10', '25', '50', '100'], showQuickJumper: false }}
+                showHeader={false}
+                rowKey="field"
+            />
+        );
+    } catch (e) {
+        return <Text code>{jsonString}</Text>;
+    }
+};
+
+export default function HistorialCambiosTable({ cambios, paciente }) {
+    const { t } = useTranslation('home');
+    const lang = t;
+    if (!cambios) {
+        return <h1>No hay datos disponibles.</h1>;
+    }
+
+    const columns = [
+        { title: 'Entidad', dataIndex: 'entidad', key: 'entidad' },
+        {
+            title: 'Fecha de Cambio', dataIndex: 'fechaCambio', key: 'fechaCambio',
+            render: (text) => {
+                const date = new Date(text);
+                return date.toLocaleString();
+            }
+        },
+        { title: 'Operación', dataIndex: 'operacion', key: 'operacion' },
+        {
+            title: 'Valor Anterior',
+            dataIndex: 'valorAnterior',
+            key: 'valorAnterior',
+            render: (text, record) => (
+                <Collapse>
+                    <Panel header="Valor Anterior" key="1">
+                        {renderJsonCell(record.valorAnterior, record.valorNuevo)}
+                    </Panel>
+                </Collapse>
+            ),
+        },
+        {
+            title: 'Valor Nuevo',
+            dataIndex: 'valorNuevo',
+            key: 'valorNuevo',
+            render: (text, record) => (
+                <Collapse>
+                    <Panel header="Valor Nuevo" key="1">
+                        {renderJsonCell(record.valorNuevo, record.valorAnterior)}
+                    </Panel>
+                </Collapse>
+            ),
+        },
+    ];
+
+    return (
+        <MenuWrapper setLang={true}>
+            <BreadCrumbPacientes idPaciente={paciente.id} page={lang('cambios')} />
+            <Table columns={columns} dataSource={cambios} rowKey="id" pagination={{ defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '25', '50', '100'], showQuickJumper: true }}
+            />
+        </MenuWrapper>
+    );
+};
+
+export const getServerSideProps = async (context) => {
+    const res = await axios.get(process.env['HOST'] + 'api/historial-cambios/listar/' + context.query.id);
+    if (res.data === null) {
+        return {
+            props: {
+                cambios: null,
+                paciente: { id: context.query.id }
+            },
+        };
+    }
+    return {
+        props: {
+            cambios: res.data,
+            paciente: { id: context.query.id }
+        },
+    };
+};
