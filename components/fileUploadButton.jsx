2c5740eafc17dc9c96781aad31a80a899f973e0c
SrChey <carlos-avb@hotmail.com>
2024-08-10 18:51:47 -0500

    resumen subida de datos

--- components/fileUploadButton.jsx
+++ components/fileUploadButton.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Upload, Button, message, Row, Col } from 'antd';
+import { Upload, Button, message, Row, Col, Modal, Card } from 'antd';
 import { FileExcelOutlined, UploadOutlined as UploadIcon } from '@ant-design/icons';
 import useTranslation from 'next-translate/useTranslation';
 
 const FileUploadButton = () => {
     const [fileList, setFileList] = useState([]);
-
+    const [modal2Open, setModal2Open] = useState(false);
+    const [modal2Content, setModal2Content] = useState([]);
     const handleUpload = async () => {
         if (fileList.length === 0) {
             message.error('No hay archivo para subir');
@@ -22,8 +23,9 @@ const FileUploadButton = () => {
                     'Content-Type': 'multipart/form-data',
                 },
             });
+            await setModal2Content(response.data);
+            await setModal2Open(true);
 
-            message.success(response.data);
         } catch (error) {
             console.error('Error al subir el archivo:', error);
             message.error('Error al subir el archivo: ' + error.message);
@@ -51,10 +53,50 @@ const FileUploadButton = () => {
         fileList,
         accept: '.xls,.xlsx', // Limita los tipos de archivos aceptados a Excel
     };
-
+    const copyContent = () => {
+        const contentToCopy = modal2Content.join('\n');
+        navigator.clipboard.writeText(contentToCopy)
+            .then(() => {
+                message.success('Contenido copiado al portapapeles');
+            })
+            .catch(() => {
+                message.error('Error al copiar el contenido');
+            });
+    };
+    const handleCloseModal = () => {
+        setModal2Open(false);
+        setModal2Content([]);
+    }
     return (
         <div className="container">
+            <Modal
+                title="Resumen de la carga"
+                centered
+                cancelButtonProps={{ style: { display: 'none' } }}
+                open={modal2Open}
+                onOk={() => setModal2Open(false)}
+                style={{ maxWidth: '500px' }}
+                footer={[
+                    <Button key="back" type="primary" onClick={() => handleCloseModal()}>
+                        Ok
+                    </Button>,
+                    <Button key="copy" onClick={copyContent}>
+                        Copiar
+                    </Button>,
+                ]}
+            >
+                <Card style={{ maxHeight: '400px', overflowY: 'auto' }}
+                >
+                    <ul>
+                        {modal2Content.map((content, index) => (
+                            <li key={index}>{content}</li>
+                        ))}
+                    </ul>
+                </Card>
+
+            </Modal>
             <Row gutter={16}>
+
                 <Col>
                     <Upload {...props}>
                         <Button icon={<FileExcelOutlined />}>{lang('Seleccionar_Archivo')}</Button>
